6dc29ff69b14fd86b133fe7b1b8502df8e943763
Austin Matzko <austin@pressedcode.com>
2021-03-31 20:12:23 -0400

    Make Kaminari pagination work with Ruby 3.0.0

--- lib/api-pagination.rb
+++ lib/api-pagination.rb
@@ -94,7 +94,7 @@ module ApiPagination
         options[:per_page] = get_default_per_page_for_kaminari(collection)
       end
 
-      collection = Kaminari.paginate_array(collection, paginate_array_options) if collection.is_a?(Array)
+      collection = Kaminari.paginate_array(collection, **paginate_array_options) if collection.is_a?(Array)
       collection = collection.page(options[:page]).per(options[:per_page])
       collection.without_count if !collection.is_a?(Array) && !ApiPagination.config.include_total
       [collection, nil]
